0f5248cac275e79bffd4e99df743c75d24497153
kikisfb <90644063+kikisfb@users.noreply.github.com>
2021-11-07 18:48:41 -0500

    I added comments and fixed the mistakes that made the tests go wrong! :)

--- CSCN71020-assign3/main.c
+++ CSCN71020-assign3/main.c
@@ -1,4 +1,9 @@
+/*
+	Kiana Safavi Baygi
+	CSCN71020-Assignment#3
+	Nov, 2021
 
+*/
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -7,7 +12,7 @@
 #pragma warning(disable:4996)
 #define MAXNUMOFCHARS 20
 
-char* RockPaperScissorsGame(char[], char[]);
+char* RockPaperScissorsGame(char[], char[]);  //introducing the function with two char array input and one char array output
 
 int main(void) {
 
@@ -18,14 +23,14 @@ int main(void) {
 	printf("Player2\n Please enter Rock, Paper or Scissor(first letter must be capital):\n");
 	scanf("%s", player2Input);
 
-	char* result=RockPaperScissorsGame(player1Input,player2Input);
+	char* result=RockPaperScissorsGame(player1Input,player2Input);//calling the function
 	printf("\nThe result is: %s\n", result);
 	return 0;
 }
 
 char* RockPaperScissorsGame(char firstInput[], char secondInput[])
 {
-	static char result[MAXNUMOFCHARS] ;
+	static char result[MAXNUMOFCHARS] ;//the returing variable
 	char Rock[] = "Rock";
 	char Paper[] = "Paper";
 	char Scissors[] = "Scissors";
@@ -33,7 +38,7 @@ char* RockPaperScissorsGame(char firstInput[], char secondInput[])
 	char Player1[] = "Player 1";
 	char Player2[] = "Player 2";
 	char Invalid[] = "Invalid";
-
+	//string length of each string
 	int DrawLength = strlen(Draw);
 	int Player1Length = strlen(Player1);
 	int Player2Length = strlen(Player2);
@@ -43,31 +48,35 @@ char* RockPaperScissorsGame(char firstInput[], char secondInput[])
 	{
 		for (int i = 0; i < DrawLength; i++)
 		{
-			result[i] = Draw[i];
+			result[i] = Draw[i];//putting each character of word Draw in ith term of result
 		}
-		
+		result[DrawLength] = '\0';//making the drawlength's term in result the last char or \0 
 	}
 	else if (((strcmp(firstInput, Rock) == 0 && strcmp(secondInput, Paper) == 0)) || ((strcmp(firstInput, Paper) == 0 && strcmp(secondInput, Scissors) == 0)) || ((strcmp(firstInput, Scissors) == 0 && strcmp(secondInput, Rock) == 0)))
 	{
 		for (int i = 0; i < Player2Length; i++)
 		{
-			result[i] = Player2[i];
+			result[i] = Player2[i];//putting each character of word Player 2 in ith term of result
 		}
+		result[Player2Length] = '\0';//making the Player2Length's term in result the last char or \0 
 	}
 	else if (((strcmp(firstInput, Rock) == 0 && strcmp(secondInput, Scissors) == 0)) || ((strcmp(firstInput, Paper) == 0 && strcmp(secondInput, Rock) == 0)) || ((strcmp(firstInput, Scissors) == 0 && strcmp(secondInput, Paper) == 0)))
 	{
 		for (int i = 0; i < Player1Length; i++)
 		{
-			result[i] = Player1[i];
+			result[i] = Player1[i];//putting each character of word Player 1 in ith term of result
 		}
+		result[Player1Length] = '\0';//making the Player1Length's term in result the last char or \0
 	}
 	else
 	{
 		for (int i = 0; i < InvalidLength; i++)
 		{
-			result[i] = Invalid[i];
+			result[i] = Invalid[i];//putting each character of word Invalid in ith term of result
 		}
+		result[InvalidLength] = '\0';//making the InvalidLength's term in result the last char or \0
 	}
+
 	return result;
 	
 }
--- RockPaperScissors_Tests/RockPaperScissors_Tests.cpp
+++ RockPaperScissors_Tests/RockPaperScissors_Tests.cpp
@@ -1,3 +1,9 @@
+/*
+	Kiana Safavi Baygi
+	CSCN71020-Assignment#3
+	Nov, 2021
+
+*/
 #include "pch.h"
 #include "CppUnitTest.h"
 extern "C"
@@ -15,65 +21,80 @@ namespace RockPaperScissorsTests
 		
 		TEST_METHOD(RockPaperScissorsGame_RockAndRock_DrawTest)
 		{
+			//testing the output of inputs Rock and Rock
 			char* result = RockPaperScissorsGame("Rock", "Rock");
 			Assert::AreEqual("Draw", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_RockAndPaper_Player2WinningTest)
 		{
+			//testing the output of inputs Rock and Paper
 			char* result = RockPaperScissorsGame("Rock", "Paper");
 			Assert::AreEqual("Player 2", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_RockAndScissors_Player1WinningTest)
 		{
+			//testing the output of inputs Rock and Scissors
 			char* result = RockPaperScissorsGame("Rock", "Scissors");
 			Assert::AreEqual("Player 1", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_PaperAndRock_Player1WinningTest)
 		{
+			//testing the output of inputs Paper and Rock
 			char* result = RockPaperScissorsGame("Paper", "Rock");
 			Assert::AreEqual("Player 1", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_PaperAndPaper_DrawTest)
 		{
+			//testing the output of inputs Paper and Paper
 			char* result = RockPaperScissorsGame("Paper", "Paper");
 			Assert::AreEqual("Draw", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_PaperAndScissors_Player1WinningTest)
 		{
+			//testing the output of inputs Paper and Scisssors
 			char* result = RockPaperScissorsGame("Paper", "Scissors");
 			Assert::AreEqual("Player 2", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_ScissorsAndPaper_Player1WinningTest)
 		{
+			//testing the output of inputs Scissors and Paper
 			char* result = RockPaperScissorsGame("Scissors", "Paper");
-			Assert::AreEqual("Player  1", result);
+			Assert::AreEqual("Player 1", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_ScissorsAndScissors_DrawTest)
 		{
+			//testing the output of inputs Scissors and Scissors
 			char* result = RockPaperScissorsGame("Scissors", "Scissors");
 			Assert::AreEqual("Draw", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_ScissorsAndRock_Player2WinningTest)
 		{
+			//testing the output of inputs Scissors and Rock
 			char* result = RockPaperScissorsGame("Scissors", "Rock");
 			Assert::AreEqual("Player 2", result);
 		}
 
 		TEST_METHOD(RockPaperScissorsGame_NotCapitalTirstLetters_InvalidInputTest)
 		{
+			//testing the output of inputs paper and rock
 			char* result = RockPaperScissorsGame("paper", "rock");
 			Assert::AreEqual("Invalid", result);
 		}
 		TEST_METHOD(RockPaperScissorsGame_AllCapitalLetters_InvalidInputTest)
 		{
+			//testing the output of inputs ROCK and SCISSORS
 			char* result = RockPaperScissorsGame("ROCK", "SCISSORS");
 			Assert::AreEqual("Invalid", result);
-		}TEST_METHOD(RockPaperScissorsGame_RandomLettersInput_InvalidInputTest)
+		}
+		TEST_METHOD(RockPaperScissorsGame_RandomLettersInput_InvalidInputTest)
 		{
+			//testing the output of inputs hi and hey
 			char* result = RockPaperScissorsGame("hey", "hi");
 			Assert::AreEqual("Invalid", result);
-		}TEST_METHOD(RockPaperScissorsGame_RandomNumbersInput_InvalidInputTest)
+		}
+		TEST_METHOD(RockPaperScissorsGame_RandomNumbersInput_InvalidInputTest)
 		{
+			//testing the output of inputs 123 and 456
 			char* result = RockPaperScissorsGame("123", "456");
 			Assert::AreEqual("Invalid", result);
 		}
